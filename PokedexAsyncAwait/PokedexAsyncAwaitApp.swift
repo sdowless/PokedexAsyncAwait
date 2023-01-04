f9f121bc0cd00a87557e44ff2d945746733eb87b
Stephan Dowless <dowless.stephan@gmail.com>
2023-01-04 11:31:38 -0500

    Initial Commit

---
+++ PokedexAsyncAwait/PokedexAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAsyncAwaitApp.swift
+//  PokedexAsyncAwait
+//
+//  Created by Stephan Dowless on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
